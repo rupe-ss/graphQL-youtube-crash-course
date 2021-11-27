752ab4c9b629d07be8137cef659feefb44995523
rupesss-09 <rupesss09@gmail.com>
2021-11-26 18:59:45 -0600

    Created models for Book and Author, need to store coming data in object.

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const schema = require("./schema");
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const schema = require('./schema/schema');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
+const mongoose = require('mongoose');
 
 app.listen(4000, () => {
-  console.log("Now listening to port 4000.");
+  console.log('Now listening to port 4000.');
+});
+
+mongoose.connect(
+  'mongodb+srv://rupess:test123@cluster0.wzxhy.mongodb.net/Cluster0?retryWrites=true&w=majority'
+);
+mongoose.connection.once('open', () => {
+  console.log('conneted to database');
 });
 
 //Binding express with GraphQL
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema: schema,
     graphiql: true,
